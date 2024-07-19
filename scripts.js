a069f4f7de1382fbc574ea25b98c48005b275a89
JavierIQ <javier.iniguez2498@gmail.com>
2024-07-19 00:32:55 -0600

    fix padding hrefs

--- scripts.js
+++ scripts.js
@@ -6,3 +6,24 @@ function switchTheme() {
     currentTheme = currentTheme % 4 + 1;
     document.body.classList.add(themes[currentTheme - 1]);
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const header = document.getElementById('header');
+    const links = document.querySelectorAll('a[href^="#"]');
+  
+    for (const link of links) {
+      link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+  
+        const headerHeight = header.offsetHeight; // Get the height of the header
+        const targetPosition = targetElement.offsetTop - headerHeight; // Calculate the new scroll position
+  
+        window.scrollTo({
+          top: targetPosition,
+          behavior: 'smooth'
+        });
+      });
+    }
+  });
